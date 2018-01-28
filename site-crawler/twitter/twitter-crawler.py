80e99a280c09209c48d79b72ff477e3b602c5519
kwoshvick <kwoshvick@gmail.com>
2018-01-28 18:55:53 +0300

    initial setup

---
+++ site-crawler/twitter/twitter-crawler.py
@@ -0,0 +1,10 @@
+import tweepy
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print (tweet.text)
