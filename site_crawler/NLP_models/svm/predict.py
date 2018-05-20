02f7c40ca16ed28848c4b468217f9c20811dc913
kwoshvick <kwoshvick@gmail.com>
2018-05-20 22:34:20 +0300

    test

--- site_crawler/NLP_models/svm/predict.py
+++ site_crawler/NLP_models/svm/predict.py
@@ -1,11 +1,16 @@
 from sklearn.externals import joblib
 model=joblib.load('model.pkl')
 
-text2 = ["the world's smallest disneyland has posted losses for 9 of the 12 years since it opened. local visitors make up 41%… ",
-         "kenya's economy struggles",
-         "loss making venture",
-         "Uchumi",
-         "nakumatt"
+text2 = [
+    # "the world's smallest disneyland has posted losses for 9 of the 12 years since it opened. local visitors make up 41%… ",
+    #      "kenya's economy struggles",
+         "loss making venture"
+    #      "Uchumi",
+    #      "nakumatt",
+    #      "Centum ",
+    #      "use becomes a public limited company"
          ]
 
-print(model.predict(text2))
+a =model.predict(text2)
+
+print(a[0])
